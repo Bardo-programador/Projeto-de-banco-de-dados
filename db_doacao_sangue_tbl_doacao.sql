8fd017f3427dd4098382ff89fa68e1c65ce5a43f
Samuel Roberto <66800900+Bardo-programador@users.noreply.github.com>
2023-01-17 11:09:04 -0300

    Versão do dia 17/01

--- db_doacao_sangue_tbl_doacao.sql
+++ db_doacao_sangue_tbl_doacao.sql
@@ -23,14 +23,14 @@ DROP TABLE IF EXISTS `tbl_doacao`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_doacao` (
-  `id_doacao` int(7) NOT NULL AUTO_INCREMENT,
-  `id_doador` int(4) NOT NULL,
+  `id_doacao` int(11) NOT NULL AUTO_INCREMENT,
+  `id_doador` int(11) NOT NULL,
   `volume_doacao` decimal(5,2) NOT NULL,
   `data_doacao` date NOT NULL,
   PRIMARY KEY (`id_doacao`),
   KEY `fk_doador` (`id_doador`),
   CONSTRAINT `fk_doador` FOREIGN KEY (`id_doador`) REFERENCES `tbl_pessoa` (`id_pessoa`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `tbl_doacao` (
 
 LOCK TABLES `tbl_doacao` WRITE;
 /*!40000 ALTER TABLE `tbl_doacao` DISABLE KEYS */;
-INSERT INTO `tbl_doacao` VALUES (1,1,354.12,'2022-12-13'),(2,1,235.12,'2021-09-11');
+INSERT INTO `tbl_doacao` VALUES (1,1,354.12,'2022-12-13'),(2,1,235.12,'2021-09-11'),(3,2,112.65,'2020-01-24'),(4,5,70.60,'2019-05-12'),(5,6,257.26,'2020-03-16'),(6,3,82.31,'2018-05-26'),(7,4,354.91,'2017-02-09'),(8,7,255.26,'2021-01-11'),(9,9,351.32,'2023-01-16'),(10,8,158.49,'2020-09-11'),(11,10,238.37,'2021-12-25'),(12,9,86.15,'2019-03-09'),(13,6,193.97,'2019-03-02'),(14,4,175.90,'2015-11-23'),(15,3,242.88,'2016-07-12'),(16,6,175.90,'2016-09-29');
 /*!40000 ALTER TABLE `tbl_doacao` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-13 11:22:12
+-- Dump completed on 2023-01-17 11:05:27
--- db_doacao_sangue_tbl_pessoa.sql
+++ db_doacao_sangue_tbl_pessoa.sql
@@ -31,7 +31,7 @@ CREATE TABLE `tbl_pessoa` (
   `telefone_pessoa` varchar(11) DEFAULT NULL,
   `telefone_fixo_pessoa` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id_pessoa`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `tbl_pessoa` (
 
 LOCK TABLES `tbl_pessoa` WRITE;
 /*!40000 ALTER TABLE `tbl_pessoa` DISABLE KEYS */;
-INSERT INTO `tbl_pessoa` VALUES (1,'Samuel','Bezerra',17,'A+','81912345678','3214-5465'),(2,'Luís','Lima',20,'B+','81998215374','7612-9472'),(4,'Luciano','José',34,'AB+','81982317853','6471-8275'),(5,'Samuel','Macedo',36,'B+',NULL,NULL),(6,'Fujiro','Kamiyamoto',100,'AB-','81921355112',NULL),(7,'Takacara','Nomuro',7,'AB-',NULL,'7212-3439');
+INSERT INTO `tbl_pessoa` VALUES (1,'Samuel','Bezerra',17,'A+','81912345678','3214-5465'),(2,'Luís','Lima',20,'B+','81998215374','7612-9472'),(3,'José','Souza',36,'O+','81936342168',''),(4,'Luciano','José',34,'AB+','81982317853','6471-8275'),(5,'Samuel','Macedo',36,'B+',NULL,NULL),(6,'Fujiro','Kamiyamoto',100,'AB-','81921355112',NULL),(7,'Takacara','Nomuro',7,'AB-',NULL,'7212-3439'),(8,'Carolina','Neves',30,'AB+','',''),(9,'Samuel','Rodrigues',27,'A-','',''),(10,'Jorge','Silva',41,'O-','71893540102','3145-5412'),(11,'Pedro','Farias',34,'B-','81934128926','3264-9812'),(12,'Cleiton','Moraes',26,'O+','82999238123','');
 /*!40000 ALTER TABLE `tbl_pessoa` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-13 11:22:12
+-- Dump completed on 2023-01-17 11:05:27
